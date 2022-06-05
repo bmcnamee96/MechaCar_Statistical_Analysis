ba4ae0f197c2766a8844feda60b7f66b1271a226
bmcnamee96 <bmac965@gmail.com>
2022-06-05 16:52:13 -0400

    linear regression

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,1 +1,5 @@
-# challenge
+# import the MechaCar_mpg.csv file
+dataframe <- read.csv(file='MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=dataframe) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=dataframe)) #generate summary statistics
